42f01706e1ac6db1d067ea477873b6322cb54e44
Jyasveer Gotta <jyasveer@gmail.com>
2018-02-17 17:52:35 +0530

    functions and arrays done

--- 3-functions/6-closures.js
+++ 3-functions/6-closures.js
@@ -5,16 +5,20 @@ add(1, 2);
 // console.log(a);
 
 function sum(a){
-    var inner = function(b){
-        return a + b;
+    var inner = function(b) {
+        console.log(a + b);
     }
     return inner;
 }
 
+// var abc = 'sum';
+// var abc = sum;
+// console.log(abc, sum);
 var abc = sum(10);
 console.log(abc); // [10, 10, undefined, undefined] => all are wrong
-var result  = abc(20);
-console.log('result', result);
+abc(10);
+// var result  = abc(20);
+// console.log('result', result);
 
 // add 3 numbers
 function sum(a){
@@ -26,6 +30,6 @@ function sum(a){
 }
 console.log(sum(10)(20)(30));
 
-function multiply(b){
-    return sum(10)(20)(30) * b;
-};
+// function multiply(b){
+//     return sum(10)(20)(30) * b;
+// };
